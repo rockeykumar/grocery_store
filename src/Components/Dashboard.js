d012fd49930c09e576d5832c555ae7f2803b9d8e
rockeykumar <rockeykumar1901@gmail.com>
2021-08-07 11:06:00 +0530

    First commit grocery store

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -153,14 +153,14 @@ const Dashboard = () => {
 
           <div className="col-md-12">
             <div className="row">
-              <div className="col">
+              <div className="col ">
                 <label htmlFor="exampleInputEmail1" className="form-label">
                   Weight(Kg.)
                 </label>
                 <div className="input-group mb-3">
                   <Select
                     className="dropDownQunatity"
-                    placeholder="Choose any one"
+                    placeholder="Select...!"
                     name="KilogramValue"
                     value={KilogramValue}
                     onChange={KilogramHandleChange}
@@ -169,14 +169,14 @@ const Dashboard = () => {
                   />
                 </div>
               </div>
-              <div className="col">
+              <div className="col ">
                 <label htmlFor="exampleInputEmail1" className="form-label">
                   Weight(gram.)
                 </label>
                 <div className="input-group mb-3">
                   <Select
                     className="dropDownQunatity"
-                    placeholder="Choose any one"
+                    placeholder="Select...!"
                     name="GramValue"
                     value={GramValue}
                     onChange={GramHandleChange}
@@ -199,13 +199,13 @@ const Dashboard = () => {
           </div>
         </div>
 
-        {/* custome / manual product here  */}
+        {/*=================== custome / manual product here =================================================================================== */}
 
         <div className="mt-4">
           <span className="display-5">Product Not In The List</span>
 
           <div className="row mt-4">
-            <div className="col-12">
+            <div className="col-12 ">
               <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">
                   Item Name
@@ -219,30 +219,88 @@ const Dashboard = () => {
                   value={useData.ItemName}
                 />
               </div>
+            </div>
+          </div>
 
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Set / Packet / Kg. / Gram
-                </label>
-                <input
-                  type="text"
-                  name="Weight"
-                  className="form-control"
-                  placeholder="eg. 2 set, 4 packet, 5 kg, 250 gram"
-                  onChange={manualInputHandle}
-                  value={useData.Weight}
-                />
-              </div>
+          <div className="row">
+            <div className=" col-lg-6 mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Weight
+              </label>
+              <input
+                type="text"
+                name="Weight"
+                className="form-control"
+                placeholder="eg. 2 set, 4 packet, 5 kg, 250 gram"
+                onChange={manualInputHandle}
+                value={useData.Weight}
+              />
+            </div>
 
-              <div className="d-grid gap-2">
-                <button
-                  className="btn btn-primary"
-                  type="button"
-                  onClick={manualAddItemButton}
-                >
-                  Manual Add Item
-                </button>
+            <div className="col-lg-6 mb-3">
+              <div className="row mt-1">
+                <div className="col-3 border-end">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Set
+                  </label>
+                  <br />
+                  <input
+                    type="radio"
+                    name="customWeight"
+                    value="set"
+                    className="mx-1"
+                  />
+                </div>
+                <div className="col-3 border-end ">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Packet
+                  </label>
+                  <br />
+                  <input
+                    type="radio"
+                    name="customWeight"
+                    value="packet"
+                    className="mx-3"
+                  />
+                </div>
+                <div className="col-3 border-end ">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Kg.
+                  </label>
+                  <br />
+                  <input
+                    type="radio"
+                    name="customWeight"
+                    value="kg"
+                    className="mx-1"
+                  />
+                </div>
+                <div className="col-3 ">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Gram
+                  </label>
+                  <br />
+                  <input
+                    type="radio"
+                    name="customWeight"
+                    value="gram"
+                    className="mx-2"
+                  />
+                </div>
               </div>
+            </div>
+          </div>
+
+          {/* button  */}
+          <div>
+            <div className="d-grid gap-2">
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={manualAddItemButton}
+              >
+                Manual Add Item
+              </button>
             </div>
           </div>
         </div>
