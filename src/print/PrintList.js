cd424f98034dad90be1a6a669bb365a31a3a41fe
rockeykumar <rockeykumar1901@gmail.com>
2021-08-15 21:04:30 +0530

    erase unused lines of code

--- src/print/PrintList.js
+++ src/print/PrintList.js
@@ -2,41 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import "../App.css";
 
-// const PrintList = () => {
-//   document.title = "Print";
-//   const products = useSelector((state) => state.allProducts.products);
-//   const renderList = products.map((product, ind) => {
-//     const { id, ItemName, Weight } = product;
-//     return (
-//       <tr key={id}>
-//         <th scope="row">{ind + 1}</th>
-//         <td>{ItemName}</td>
-//         <td>{Weight}</td>
-//       </tr>
-//     );
-//   });
-
-//   return (
-//     <>
-//       <div className="container" ref={ref}>
-//         <div className="row mt-0">
-//           <div className="col text-center display-5">!! नराया जी !!</div>
-//         </div>
-//         <table className="table table-striped mt-3">
-//           <thead>
-//             <tr>
-//               <th scope="col">#</th>
-//               <th scope="col">Item Name</th>
-//               <th scope="col">Kg./Gram.</th>
-//             </tr>
-//           </thead>
-//           <tbody>{renderList}</tbody>
-//         </table>
-//       </div>
-//     </>
-//   );
-// };
-
 const PrintList = React.forwardRef((props, ref) => {
   const products = useSelector((state) => state.allProducts.products);
   //   const date = new Date();
